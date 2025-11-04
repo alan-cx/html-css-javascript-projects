68a5984c60bf142ea6d8815f2303e698b42ea08d
alan-cx <alan.chp10@gmail.com>
2025-11-04 17:35:46 -0500

    Counter

--- 01.counter/assets/js/index.js
+++ 01.counter/assets/js/index.js
@@ -1,26 +1,26 @@
 
-const incrementBtn = document.querySelector('.increment')
-const decrementBtn = document.querySelector('.decrement')
-const resetBtn = document.querySelector('.reset')
-const counterValue = document.querySelector('.show')
+const incrementBtn = document.querySelector('.increment');
+const decrementBtn = document.querySelector('.decrement');
+const resetBtn = document.querySelector('.reset');
+const counterValue = document.querySelector('.show');
 
-let count = 0
+let count = 0;
 
 function increment() {
-    count += 1
-    counterValue.innerText = count
+  count += 1;
+  counterValue.innerText = count;
 }
 
 function decrement() {
-    count -= 1
-    counterValue.innerText = count
+  count -= 1;
+  counterValue.innerText = count;
 }
 
 function reset() {
-    count -= count
-    counterValue.innerText = count
+  count -= count;
+  counterValue.innerText = count;
 }
 
-incrementBtn.addEventListener( 'click', increment )
-decrementBtn.addEventListener( 'click', decrement )
-resetBtn.addEventListener( 'click', reset )
+incrementBtn.addEventListener('click', increment);
+decrementBtn.addEventListener('click', decrement);
+resetBtn.addEventListener('click', reset);
