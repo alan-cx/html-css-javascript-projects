add1953684645a1ff8c0ad9d31ee950b7b2b9468
alan-cx <alan.chp10@gmail.com>
2025-11-20 20:02:53 -0500

    06.mouse-event

--- 06.mouse-event/assets/js/app.js
+++ 06.mouse-event/assets/js/app.js
@@ -1,10 +1,10 @@
 const showX = document.querySelector('.show-x-value')
 const showY = document.querySelector('.show-y-value')
-const ball = document.querySelector('.bolita')
+const ball = document.querySelector('.round')
 
 document.addEventListener('mousemove', (e) => {
-    showX.innerText = `${ e.clientX }`
-    showY.innerText = `${ e.clientY }`
-    ball.style.top = `${ e.clientY }px`
-    ball.style.left = `${ e.clientX }px`
+  showX.innerText = `${e.clientX}`
+  showY.innerText = `${e.clientY}`
+  ball.style.top = `${e.clientY}px`
+  ball.style.left = `${e.clientX}px`
 })
